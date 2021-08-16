a8a36413edda3d3d878a9ef27fe81490f5d77c2c
Ghost120494 <88986034+Ghost120494@users.noreply.github.com>
2021-08-15 20:38:41 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,9 +1,22 @@
 var gameData = {
-    gold: 0,
-    goldPerClick: 1
+  gold: 0,
+  goldPerClick: 1,
+  goldPerClickCost: 100
 }
   
   function mineGold() {
     gameData.gold += gameData.goldPerClick
     document.getElementById("goldMined").innerHTML = gameData.gold + " Existential Crises"
   }
+  function buyGoldPerClick() {
+    if (gameData.gold >= gameData.goldPerClickCost) {
+      gameData.gold -= gameData.goldPerClickCost
+      gameData.goldPerClick += 1
+      gameData.goldPerClickCost *= 2
+      document.getElementById("goldMined").innerHTML = gameData.gold + "Existential Crises"
+    document.getElementById("perClickUpgrade").innerHTML = "Read Nietzche (+1 Crisis/second " + gameData.goldPerClick + ") Cost: " + gameData.goldPerClickCost + " Gold"
+    }
+  }
+  var mainGameLoop = window.setInterval(function() {
+    mineGold()
+  }, 1000)
